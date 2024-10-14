316816a78c2eb6ad83a441b51951953df11050b7
Vikash Thakur <thakvikash622@gmail.com>
2024-10-14 23:26:12 +0530

    Fixed: Loadpage

--- app/action.ts
+++ app/action.ts
@@ -1,9 +1,9 @@
 'use server'
 
-export const fetchAnime = async () => {
-    const response = await fetch(`https://shikimori.one/api/animes?page=1&limit=8&order=popularity`);
+export const fetchAnime = async (page: number) => {
+    const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
     const data = await response.json();
-    console.log("Data are:", data)
+    // console.log("Data are:", data)
     return data;
 
 }
